5e3086f412c1b3f8cd39b32704b748e624e1b73e
Bastien Soucasse <bastien.soucasse@etu.u-bordeaux.fr>
2022-12-21 21:14:28 +0100

    Train 4 SR models on 2 epochs only (also fix display utility, fix gitignore, and fix SR Jupyter notebook)

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,36 @@ import torchvision.transforms as transforms
 from PIL import Image
 
 
+def display_image(image: Image.Image) -> None:
+    """Displays an image."""
+
+    # Display the image in the plot
+    plt.imshow(image)
+    plt.axis("off")
+
+    # Show the figure
+    plt.show()
+
+
+def display_tensors(tensor: torch.Tensor) -> None:
+    """Displays an image tensor."""
+
+    # Convert tensor to image
+    image = transforms.ToPILImage()(tensor)
+
+    # Call the display image utility
+    display_image(image)
+
+
 def display_images(images: "list[Image.Image]") -> None:
     """Displays a list of images side to side."""
+
+    if len(images) == 0:
+        return
+
+    if len(images) == 1:
+        display_image(images[0])
+        return
 
     # Create a figure with two subplots
     _, axes = plt.subplots(1, len(images), figsize=(4 * len(images), 4))
